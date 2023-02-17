848672b6feae4160b75510e517c1fa8062399768
guigattidev <gattiwhoomps@hotmail.com>
2023-02-17 15:15:03 -0300

    feat add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("typescript");
+import express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const server = express();
+
+server.get("/", (req, res) => {
+    res.send("Hello world");
+});
+
+server.listen(process.env.APP_PORT, () => {
+    console.log("Server listening on port " + process.env.APP_PORT);
+});
